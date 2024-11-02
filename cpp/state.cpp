62c1fe86acfc93e67b3bda0da61f0be7bd3d2e39
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-11-02 21:32:08 +0700

    feat: add state essential methods

--- cpp/state.cpp
+++ cpp/state.cpp
@@ -1,4 +1,116 @@
 #include "state.hpp"
+#include <cstdlib>   // random
+#include <ctime>     // time
+#include <algorithm> // shuffle
+#include <sstream>   // tostring
+
+int getVectorIndex(int i,int j,int k){
+    return (25*i+5*j+k);
+}
+
+/*-------------------- CONSTRUCTOR -----------------------*/
+State::State()
+{
+    buffer[0] = vector<int>(25, 0);
+    buffer[1] = vector<int>(25, 0);
+    buffer[2] = vector<int>(25, 0);
+    buffer[3] = vector<int>(30, 0);
+    buffer[4] = vector<int>(4, 0);
+
+    for (int i = 0; i < 125; i++)
+    {
+        matriks.push_back(i+1);
+    }
+
+    srand(static_cast<unsigned>(time(0)));
+    random_shuffle(matriks.begin(), matriks.end());
+
+    this->calculateBuffer();
+    this->val = this->objectiveFunction();
+}
+
+State::State(vector<int>& m)
+{
+    matriks = m;    
+
+    buffer[0] = vector<int>(25, 0);
+    buffer[1] = vector<int>(25, 0);
+    buffer[2] = vector<int>(25, 0);
+    buffer[3] = vector<int>(30, 0);
+    buffer[4] = vector<int>(4, 0);
+
+    this->calculateBuffer();
+    this->val = this->objectiveFunction();
+}
+
+State::State(const State &state) : val(state.val)
+{
+    matriks = state.matriks;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        buffer[i] = state.buffer[i];
+    }
+}
+
+/*-------------------- GETTER/SETTER -----------------------*/
+
+int State::getElement(int i, int j, int k)
+{
+    int idx = getVectorIndex(i,j,k);
+    return this->matriks[idx];
+}
+
+void State::setElement(int i, int j, int k, int val)
+{
+    int idx = getVectorIndex(i,j,k);
+    this->matriks[idx] = val;
+}
+
+int State::getStateValue()
+{
+    return this->val;
+}
+
+vector<int> State::getMatrix(){
+    return this->matriks;
+}
+
+/*-------------------- OUTPUT DEBUG -----------------------*/
+string State::toString()
+{
+    std::ostringstream oss;
+    int idx = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        oss << "Layer " << i + 1 << ":\n";
+        for (int j = 0; j < 5; ++j)
+        {
+            for (int k = 0; k < 5; ++k)
+            {
+                oss << matriks[idx] << "\t";
+                idx++;
+            }
+            oss << "\n";
+        }
+        oss << "\n";
+    }
+    return oss.str();
+}
+
+void State::printBuffer()
+{
+    for (vector vec : this->buffer)
+    {
+        for (int segment : vec)
+        {
+            cout << segment << " ";
+        }
+        cout << endl;
+    }
+}
+
+/*-------------------- HELPER DIAGONAL -----------------------*/
 
 vector<int> State::diaBidangIndexes(int i, int j, int k)
 {
@@ -39,41 +151,33 @@ vector<int> State::diaBidangIndexes(int i, int j, int k)
     return bidang_indexes;
 }
 
-vector<int> State::diaRuangIndexes(int i,int j,int k){
+vector<int> State::diaRuangIndexes(int i, int j, int k)
+{
     vector<int> ruang_indexes;
-    if(i==j && j==k){
+    if (i == j && j == k)
+    {
         ruang_indexes.push_back(0);
     }
 
-    if((4-i)==j && j==k){
+    if ((4 - i) == j && j == k)
+    {
         ruang_indexes.push_back(1);
     }
 
-    if(i==(4-j) && i==k){
+    if (i == (4 - j) && i == k)
+    {
         ruang_indexes.push_back(2);
     }
 
-    if(i==j && j==(4-k)){
+    if (i == j && j == (4 - k))
+    {
         ruang_indexes.push_back(3);
     }
 
     return ruang_indexes;
 }
 
-void State::calculateBuffer()
-{
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            for (int k = 0; k < 4; k++)
-            {
-                this->updateBuffer(i, j, k, this->matriks[i][j][k], 0);
-            }
-        }
-    }
-    this->val = this->objectiveFunction();
-}
+/*-------------------- BUFFER METHODS -----------------------*/
 
 void State::updateBuffer(int i, int j, int k, int newVal, int oldVal)
 {
@@ -84,11 +188,16 @@ void State::updateBuffer(int i, int j, int k, int newVal, int oldVal)
     }
 
     int row_idx = i + 5 * k;
+
+    // if(row_idx==0){
+    //     cout<<i<<" "<<j<<" "<<k<<endl;
+    // }
+
     buffer[0][row_idx] += diffVal;
     if (buffer[0][row_idx] == MAGIC_NUMBER)
         this->val += 1;
     if (buffer[0][row_idx] - diffVal == MAGIC_NUMBER)
-        this->val -= 0;
+        this->val -= 1;
 
     int col_idx = j + 5 * k;
     buffer[1][col_idx] += diffVal;
@@ -98,6 +207,9 @@ void State::updateBuffer(int i, int j, int k, int newVal, int oldVal)
         this->val -= 1;
 
     int tiang_idx = i + 5 * j;
+    // if(tiang_idx==0){
+    //     cout<<i<<" "<<j<<" "<<k<<endl;
+    // }
     buffer[2][tiang_idx] += diffVal;
     if (buffer[2][tiang_idx] == MAGIC_NUMBER)
         this->val += 1;
@@ -105,16 +217,27 @@ void State::updateBuffer(int i, int j, int k, int newVal, int oldVal)
         this->val -= 1;
 
     vector<int> bidang_indexes = this->diaBidangIndexes(i, j, k);
+
+    // if(find(bidang_indexes.begin(),bidang_indexes.end(), 0) != bidang_indexes.end()){
+    //     cout<<i<<" "<<j<<" "<<k<<endl;
+    // }
+
     for (int bidang_idx : bidang_indexes)
     {
         this->buffer[3][bidang_idx] += diffVal;
         if (buffer[3][bidang_idx] == MAGIC_NUMBER)
             this->val += 1;
         if (buffer[3][bidang_idx] - diffVal == MAGIC_NUMBER)
+
             this->val -= 1;
     }
 
     vector<int> ruang_indexes = this->diaRuangIndexes(i, j, k);
+
+    // if(find(ruang_indexes.begin(),ruang_indexes.end(), 2) != ruang_indexes.end()){
+    //     cout<<i<<" "<<j<<" "<<k<<endl;
+    // }
+
     for (int idx : ruang_indexes)
     {
         this->buffer[4][idx] += diffVal;
@@ -125,71 +248,112 @@ void State::updateBuffer(int i, int j, int k, int newVal, int oldVal)
     }
 }
 
-State::State(){
-    vector<int> numbers;
-    for (int i = 1; i <= 125; ++i) {
-        numbers.push_back(i);
-    }
-
-    srand(static_cast<unsigned>(time(0))); 
-    random_shuffle(numbers.begin(), numbers.end());
-
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            for (int k = 0; k < 5; ++k) {
-                matriks[i][j][k] = numbers[i * 25 + j * 5 + k];
+void State::calculateBuffer()
+{
+    int idx = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                this->updateBuffer(i, j, k, this->matriks[idx], 0);
+                idx++;
             }
         }
-        buffer[i].clear();
     }
-    // calculateBuffer();
-    // value = objectiveFunction();
+    this->val = this->objectiveFunction();
 }
 
-State::State(int m[5][5][5]){
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            for (int k = 0; k < 5; ++k) {
-                matriks[i][j][k] = m[i][j][k];
-            }
-        }
-        buffer[i].clear();
-    }
-    // calculateBuffer();
-    // value = objectiveFunction();
-}
+/*-------------------- OBJECTIVE FUNCTIOn -----------------------*/
 
-int State::objectiveFunction(){
+int State::objectiveFunction()
+{
     int result = 0;
-    // buffer[0] = baris 25
-    // buffer[1] = kolom 25
-    // buffer[2] = tiang 25
-    // buffer[3] = diagonal bidang 30
-    // buffer[4] = diagonal ruang 4 
-
-    for (vector vec : this->buffer) {
-        for (int segment : vec) {
-            if(segment == 315){
-                result ++;
+    for (vector vec : this->buffer)
+    {
+        for (int segment : vec)
+        {
+            if (segment == 315)
+            {
+                result++;
             }
         }
     }
     return result;
 }
 
-string State::toString() {
-    std::ostringstream oss;
-    for (int i = 0; i < 5; ++i) {
-        oss << "Layer " << i + 1 << ":\n";
-        for (int j = 0; j < 5; ++j) {
-            for (int k = 0; k < 5; ++k) {
-                oss << matriks[i][j][k] << "\t";
-            }
-            oss << "\n";
-        }
-        oss << "\n";
-    }
-    return oss.str();
+/*-------------------- SUCCESSOR METHODS -----------------------*/
+
+State State::generateSucc(int i1, int i2, int j1, int j2, int k1, int k2)
+{
+    State succ(*this);
+    int val1 = this->getElement(i1, j1, k1), val2 = this->getElement(i2, j2, k2);
+    succ.setElement(i1, j1, k1, val2);
+    succ.setElement(i2, j2, k2, val1);
+
+    succ.updateBuffer(i1, j1, k1, val2, val1);
+    succ.updateBuffer(i2, j2, k2, val1, val2);
+
+    return succ;
 }
 
+State State::highestValuedSucc()
+{
+    State bestState = *this;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                for (int l = (25 * i + 5 * j + k + 1); l < 125; l++)
+                {
+                    int iSucc = l / 25, jSucc = (l % 25) / 5, kSucc = l % 5;
+                    State succ = this->generateSucc(i, iSucc, j, jSucc, k, kSucc);
+                    if (succ.getStateValue() > bestState.getStateValue())
+                    {
+                        bestState = succ;
+                    }
+                }
+            }
+        }
+    }
 
+    return bestState;
+}
+
+State State::randomSucc()
+{
+    srand(time(0));
+
+    int arr1[3], arr2[3];
+    do
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            arr1[i] = rand() % 5;
+            arr2[i] = rand() % 5;
+        }
+    } while (!(arr1[0] == arr2[0] && arr1[1] == arr2[1] && arr1[2] == arr2[2]));
+
+    State neighbor = this->generateSucc(arr1[0], arr2[0], arr1[1], arr2[1], arr1[2], arr2[2]);
+
+    return neighbor;
+}
+
+/*-------------------- OPERATOR OVERLOADING -----------------------*/
+
+State &State::operator=(const State &other)
+{
+    if (this != &other)
+    {
+        matriks = other.matriks;
+        val = other.val;
+        for (int i = 0; i < 5; i++)
+        {
+            buffer[i] = other.buffer[i];
+        }
+    }
+    return *this;
+}
--- cpp/state.hpp
+++ cpp/state.hpp
@@ -5,20 +5,37 @@ using namespace std;
 
 #define MAGIC_NUMBER 315
 
-class State {
-private:    
-    int matriks[5][5][5];
+class State
+{
+private:
+    vector<int> matriks;
     vector<int> buffer[5];
     int val;
+
 public:
     State();
+    State(vector<int>&);
+    State(const State &);
+
+    int getStateValue();
+    int getElement(int, int, int);
+    void setElement(int, int, int, int);
+    vector<int> getMatrix();
+
     string toString();
-    State(int m[5][5][5]);
-    int objectiveFunction();
+    void printBuffer();
+
+    vector<int> diaBidangIndexes(int, int, int);
+    vector<int> diaRuangIndexes(int, int, int);
+
     void calculateBuffer();
-    void updateBuffer(int,int,int,int,int);
+    void updateBuffer(int, int, int, int, int);
+
+    int objectiveFunction();
+
+    State generateSucc(int, int, int, int, int, int);
     State highestValuedSucc();
     State randomSucc();
-    vector<int> diaBidangIndexes(int,int,int);
-    vector<int> diaRuangIndexes(int,int,int);
+
+    State &operator=(const State &);
 };
