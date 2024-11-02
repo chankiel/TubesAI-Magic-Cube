5d1cba38e7d08e8f4880670217e2dadafa12994a
unknown <danielmuliaputraolo@gmail.com>
2024-11-02 22:40:09 +0700

    feat: process

--- src/components/2DRender.js
+++ src/components/2DRender.js
@@ -1,10 +1,10 @@
 import React from "react";
 import D2 from "./2D";
 
-const D2Render = ({ array }) => {
+const D2Render = ({ array, idx1, idx2 }) => {
   return (
     <div className="w-full h-full">
-      <D2 array={array} />
+      <D2 array={array} idx1={idx1} idx2={idx2} />
     </div>
   );
 };
